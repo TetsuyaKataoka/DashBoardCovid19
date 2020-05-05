1b7d8e5184d5f173506ca46994f0e1ea36e160f4
Tetsuya Kataoka <tetsuya.kataoka@outlook.jp>
2020-05-06 03:55:56 +0900

    csrf コメントアウト

--- DashBoardCovid19/settings.py
+++ DashBoardCovid19/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',  # デプロイ用に追加
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
